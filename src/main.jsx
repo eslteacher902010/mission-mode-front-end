25825121d14e615045171eb8bd6cbffc93236918
paul_kniaz <pjkniaz@gmail.com>
2025-12-31 11:35:42 -0500

    Log VITE_API_URL at build time

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+console.log("BUILD ENV:", import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
